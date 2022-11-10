c03ec6a858d208d4879a8e6bea4a52c0d24d7e3f
arthurcaraujo <arthur-lagamar@hotmail.com>
2022-11-10 14:15:59 -0300

    Adding website

---
+++ script.js
@@ -0,0 +1,15 @@
+const menuBtnOne = document.getElementById("btn-1");
+const menuBtnTwo = document.getElementById("btn-2");
+const navBar = document.querySelector("nav ul");
+
+function openNav() {
+    menuBtnOne.style.display = "none";
+    menuBtnTwo.style.display = "block";
+    navBar.style.visibility = "visible";
+}
+
+function closeNav() {
+    menuBtnOne.style.display = "block";
+    menuBtnTwo.style.display = "none";
+    navBar.style.visibility = "hidden";
+}
